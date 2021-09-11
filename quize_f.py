3fff6e7ab3180f42f74963fa0b0997497769a8a0
abdullahi-7 <abdullahi.abdi@protonmail.com>
2021-09-11 12:29:22 +0300

    print is show something you write

--- quize_f.py
+++ quize_f.py
@@ -1,8 +1,11 @@
 #abdullahi, 10, kenya
 
 def quize():
-    v = int(input("Do you want to play? "))
+    v = input("Do you want to play? ")
     a = "yes"
 
     if v == a:
         print("les tart to play!!!!!!.")
+    else:
+        print("you are done!!!!")
+        quit()
